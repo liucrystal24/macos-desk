0679a541a23a14bddb87cf0bd66ce0e1013ec5fb
liucrystal24 <liucrystal_24@outlook.com>
2020-07-12 22:59:24 +0800

    modal review

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -14,10 +14,24 @@ type Props = {
   };
 };
 
+/**
+ * 父元素需要传值
+ * {
+    children，
+    data：{
+      width；height；
+      id：主体dom（Drawing）；
+      moveId：可拖拽部分（TitleBar）;
+      isshow：是否显示
+    }
+  }
+ */
+
 const Modal = React.memo(({ children, data }: Props) => {
   const domEl = document.getElementById("main-view") as HTMLDivElement;
   if (!domEl) return null;
 
+  // Portal 弹窗
   return ReactDOM.createPortal(
     <Draggable domEl={domEl} data={data}>
       {children}
