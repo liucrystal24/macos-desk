f15563e5e94c1a76326ba5108132c5f4d8cca6e1
liucrystal24 <liucrystal_24@outlook.com>
2020-08-08 14:30:42 +0000

    Deploying to gh-pages from master fb9a050ff1d313b4e3344a4c03c67b9ce3253d52

---
+++ precache-manifest.b37a84ebb23c4e29ad57b9b747989434.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7f742b295a8d9497648103aae3b79e",
+    "url": "/macos-desk/index.html"
+  },
+  {
+    "revision": "7dc14126d3540a3e7394",
+    "url": "/macos-desk/static/css/main.ab0a6fbb.chunk.css"
+  },
+  {
+    "revision": "741867cbe980700ab677",
+    "url": "/macos-desk/static/js/2.d6f9fc38.chunk.js"
+  },
+  {
+    "revision": "846e3710ca5ee3f911296f05447fe08c",
+    "url": "/macos-desk/static/js/2.d6f9fc38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc14126d3540a3e7394",
+    "url": "/macos-desk/static/js/main.a08def72.chunk.js"
+  },
+  {
+    "revision": "b710664878f32b045c98",
+    "url": "/macos-desk/static/js/runtime-main.91beae61.js"
+  },
+  {
+    "revision": "bb9998c1b7d78691c322e2fac2397439",
+    "url": "/macos-desk/static/media/Calculator.bb9998c1.png"
+  },
+  {
+    "revision": "44b3c5fc685002705ce970f61a06ac5e",
+    "url": "/macos-desk/static/media/Chrome.44b3c5fc.png"
+  },
+  {
+    "revision": "336727e0b3a0e546d40433adf66a7013",
+    "url": "/macos-desk/static/media/Drawing.336727e0.png"
+  },
+  {
+    "revision": "e7ddfa6baa98a887738f71d6cbd485a9",
+    "url": "/macos-desk/static/media/Finder.e7ddfa6b.png"
+  },
+  {
+    "revision": "dab98b41d6541727e697cda8f61b10ff",
+    "url": "/macos-desk/static/media/Launchpad.dab98b41.png"
+  },
+  {
+    "revision": "64914db7c679d32e7e5399bd66d5f67c",
+    "url": "/macos-desk/static/media/PrefApp.64914db7.png"
+  },
+  {
+    "revision": "148e3c64ba205ddb3d979487d7cd4490",
+    "url": "/macos-desk/static/media/Terminal.148e3c64.png"
+  },
+  {
+    "revision": "e314cff8defe3c70dedf38a93f1f32a0",
+    "url": "/macos-desk/static/media/gougu.e314cff8.jpg"
+  },
+  {
+    "revision": "f9f13a3aef74c290c5c9967dbeff7e67",
+    "url": "/macos-desk/static/media/wallpaper.f9f13a3a.jpg"
+  },
+  {
+    "revision": "2863d56be1f7eed03d4536be5f4ae698",
+    "url": "/macos-desk/static/media/wallpaper_small.2863d56b.jpg"
+  }
+]);
